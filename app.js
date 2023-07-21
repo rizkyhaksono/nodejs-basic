56149c3f9295d1cda030b3ad465eed36cdaa130c
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-07-21 20:58:03 +0700

    add: event listener

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const server = new Server({
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'dicoding.com',
-});
+// const server = new Server({
+//     host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'dicoding.com',
+// });
+
+const { coffee } = require("./lib/coffee");
+
+// Linux
+// NODE_ENV=production node app.js
+
+// Windows
+// SET NODE_ENV=production && node app.js
+
+console.log(coffee.name);
