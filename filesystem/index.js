664ddd75f2505009699e6802844e35b51b91412b
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-07-21 22:11:18 +0700

    fix: fix path

--- filesystem/index.js
+++ filesystem/index.js
@@ -1,16 +1,12 @@
 const fs = require('fs');
-
-// const data = fs.readFileSync('note.txt', 'utf-8');
-
-// console.log(data);
+const { resolve } = require('path');
 
 const fileReadCallback = (error, data) => {
-    if(error) {
-        console.log('Gagal membaca berkas');
-        console.log(error);
-        return;
-    }
-    console.log(data);
+  if(error) {
+    console.log('Gagal membaca berkas');
+    return;
+  }
+  console.log(data);
 };
- 
-fs.readFile('note.txt', 'UTF-8', fileReadCallback);
+
+fs.readFile(resolve(__dirname, 'note.txt'), 'UTF-8', fileReadCallback);
